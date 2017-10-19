5fb569c3039e070c68696a1bb27f21d7381cf711
Ryan David Sheasby <ryan27968@gmail.com>
2017-10-19 19:06:58 +0200

    Added dotnet core project components.

---
+++ expert_system/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace expert_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var str in args)
+                Console.WriteLine(str);
+        }
+    }
+}
